91d28d2bb3cc9be8e558fd5e9bb2915eb890c95c
Efim_Golovin <Efim_Golovin@epam.com>
2018-08-07 15:29:05 +0300

    Requirements installing.

--- create_env.sh
+++ create_env.sh
@@ -6,10 +6,10 @@ echo "But first let's check if we have right setup here:"
 
 echo "Now show me the version of tensorflow:"
 
-python -c 'import tensorflow as tf; print(tf.__VERSION__)'
+python -c 'import tensorflow as tf; print(tf.__version__)'
 
 echo "Ok, show me the version of keras:"
 
-python -c 'import keras; print(keras.__VERSION__)'
+python -c 'import keras; print(keras.__version__)'
 
 echo "Good, looks like everything is more or less fine."
