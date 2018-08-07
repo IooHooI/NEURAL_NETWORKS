eaebaad6423613e9a2221c303cd3497594aa9944
Efim_Golovin <Efim_Golovin@epam.com>
2018-08-07 15:22:55 +0300

    Requirements installing.

--- create_env.sh
+++ create_env.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Here I want the magic of Data Science to be happening!!!"
+
+echo "But first let's check if we have right setup here:"
+
+echo "Now show me the version of tensorflow:"
+
+python -c 'import tensorflow as tf; print(tf.__VERSION__)'
+
+echo "Ok, show me the version of keras:"
+
+python -c 'import keras; print(keras.__VERSION__)'
+
+echo "Good, looks like everything is more or less fine."
