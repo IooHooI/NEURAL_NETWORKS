e79095729eb5c3a4373a5b284fd56c1aa82a14d7
Efim_Golovin <Efim_Golovin@epam.com>
2018-08-14 18:42:30 +0300

    Data loader.

--- source/code/tensorflow/titanic.py
+++ source/code/tensorflow/titanic.py
@@ -1,18 +1,17 @@
 import pandas as pd
-from source.code.utils import generate_cat_feature_counts
-from source.code.utils import generate_features_names
-from source.code.utils import generate_binarized_pipeline
+from source.code.ItemSelector import ItemSelector
 
 from sklearn.pipeline import FeatureUnion
 from sklearn.pipeline import Pipeline
-from source.code.ItemSelector import ItemSelector
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
 
 
 def read_and_clean_the_data():
     titanic = pd.read_excel('../../../data/dataset/titanic3.xls')
 
     titanic.age.fillna(titanic.age.mean(), inplace=True)
-    titanic.fare.fillna(titanic.age.mean(), inplace=True)
+    titanic.fare.fillna(titanic.fare.mean(), inplace=True)
 
     titanic.sex.replace({'male': 0, 'female': 1}, inplace=True)
     titanic.embarked.replace({'S': 0, 'C': 1, 'Q': 2}, inplace=True)
@@ -20,24 +19,25 @@ def read_and_clean_the_data():
     titanic = titanic[~titanic.embarked.isnull()]
 
     num_features = ['age', 'fare']
-    cat_features = ['pclass', 'sibsp', 'parch', 'embarked']
+    cat_features = ['pclass', 'embarked', 'parch', 'sibsp']
+    # , 'embarked', 'parch', 'sibsp'
     bin_features = ['sex']
 
     X = titanic[num_features + cat_features + bin_features]
 
     pipeline = Pipeline([
         ('union', FeatureUnion(
-            [('bin', Pipeline([('choose', ItemSelector(bin_features))]))] + \
-            list(map(generate_binarized_pipeline, cat_features)) + \
+            [('bin', Pipeline([('choose', ItemSelector(bin_features))]))] +
+            # list(map(generate_binarized_pipeline, cat_features)) +
             [('num', Pipeline([('choose', ItemSelector(num_features))]))]
-        ))
+        )),
+        ('poly', PolynomialFeatures(degree=2)),
+        ('scale', StandardScaler())
     ])
 
-    ext_features = generate_features_names(bin_features, generate_cat_feature_counts(X, cat_features), num_features)
+    X = pipeline.fit_transform(X)
 
-    X = pd.DataFrame(pipeline.fit_transform(X), columns=ext_features)
+    y = titanic.survived.values
+    y = y.reshape([len(y), 1])
 
-    Y = titanic.survived
-
-    return X, Y
-
+    return X, y
