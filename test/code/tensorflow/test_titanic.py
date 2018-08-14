3b72b6fa42444aea393a06c094fc61b5d660a8ae
Efim_Golovin <Efim_Golovin@epam.com>
2018-08-14 18:43:49 +0300

    Tf classifier testing.

--- test/code/tensorflow/test_titanic.py
+++ test/code/tensorflow/test_titanic.py
@@ -1,9 +1,52 @@
 import unittest
 from source.code.tensorflow.titanic import read_and_clean_the_data
+from source.code.tensorflow.TfBinaryClassifier import TfBinaryClassifier
+
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.model_selection import train_test_split
 
 
 class TestTitanic(unittest.TestCase):
 
     def test_read_and_clean_the_data(self):
-        X, Y = read_and_clean_the_data()
-        self.assertEquals(len(X), len(Y))
+        X, y = read_and_clean_the_data()
+        self.assertEquals(len(X), len(y))
+
+    def test_tf_binary_classifier_predict(self):
+        X, y = read_and_clean_the_data()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42, stratify=y)
+
+        classifier = TfBinaryClassifier(
+            checkpoint_dir='../../../data/dataset/tf_model',
+            n_epochs=60,
+            learning_rate=0.02
+        )
+        classifier.fit(X_train, y_train)
+
+        y_pred = classifier.predict(X_test, y_test)
+
+        self.assertEquals(len(y_test[:, 0].tolist()), len(y_pred))
+
+        print('Accuracy: {}'.format(accuracy_score(y_test[:, 0].tolist(), y_pred)))
+        print('Precision: {}'.format(precision_score(y_test[:, 0].tolist(), y_pred)))
+        print('Recall: {}'.format(recall_score(y_test[:, 0].tolist(), y_pred)))
+
+    def test_tf_binary_classifier_predict_proba(self):
+        X, y = read_and_clean_the_data()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42, stratify=y)
+
+        classifier = TfBinaryClassifier(
+            checkpoint_dir='../../../data/dataset/tf_model',
+            n_epochs=60,
+            learning_rate=0.01
+        )
+        classifier.fit(X_train, y_train)
+
+        y_pred = classifier.predict_proba(X_test, y_test)
+
+        self.assertEquals(len(y_test[:, 0].tolist()), len(y_pred))
+
+        print('Roc-Auc: {}'.format(roc_auc_score(y_test[:, 0].tolist(), y_pred)))
