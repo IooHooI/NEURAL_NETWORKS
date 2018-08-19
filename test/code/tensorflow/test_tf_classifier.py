05b00b5474d64aad96f245eacf95010d378f211d
Efim Golovin <feanoref@gmail.com>
2018-08-20 02:10:26 +0300

    Refactoring.

--- test/code/tensorflow/test_tf_classifier.py
+++ test/code/tensorflow/test_tf_classifier.py
@@ -2,8 +2,6 @@ import unittest
 
 import numpy as np
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 
@@ -50,8 +48,6 @@ class TestTfClassifier(unittest.TestCase):
         self.assertEquals(len(y_test), len(y_pred))
 
         print('Accuracy: {}'.format(accuracy_score(y_test[:, 0].tolist(), y_pred[:, 0].tolist())))
-        print('Precision: {}'.format(precision_score(y_test[:, 0].tolist(), y_pred[:, 0].tolist())))
-        print('Recall: {}'.format(recall_score(y_test[:, 0].tolist(), y_pred[:, 0].tolist())))
 
     def test_tf_binary_classification_predict_proba(self):
         y_test, y_pred = predict_proba_case('binary', read_and_clean_titanic_data)
@@ -67,10 +63,6 @@ class TestTfClassifier(unittest.TestCase):
         self.assertEquals(len(y_test), len(y_pred))
 
         print('Accuracy: {}'.format(accuracy_score(np.argmax(y_test, 1), y_pred)))
-        print('Macro Precision: {}'.format(precision_score(np.argmax(y_test, 1), y_pred, average='macro')))
-        print('Macro Recall: {}'.format(recall_score(np.argmax(y_test, 1), y_pred, average='macro')))
-        print('Micro Precision: {}'.format(precision_score(np.argmax(y_test, 1), y_pred, average='micro')))
-        print('Micro Recall: {}'.format(recall_score(np.argmax(y_test, 1), y_pred, average='micro')))
 
     def test_tf_multi_classification_predict_proba(self):
         y_test, y_pred = predict_proba_case('multi', read_and_clean_thyroid_data)
